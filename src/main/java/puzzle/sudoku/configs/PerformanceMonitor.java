ccc72c1d4c16c0d73146fe8fdf432ba807b1a656
mishoto <mihail.dev.io@gmail.com>
2023-04-25 10:01:58 +0300

    add performance monitoring for sudoku solver method

---
+++ src/main/java/puzzle/sudoku/configs/PerformanceMonitor.java
@@ -0,0 +1,32 @@
+package puzzle.sudoku.configs;
+
+
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.interceptor.PerformanceMonitorInterceptor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class PerformanceMonitor {
+
+    @Pointcut("execution(public int[][] puzzle.sudoku.utils.SudokuSolver.solveSudoku(..))")
+    public void monitor(){};
+
+    @Bean
+    public PerformanceMonitorInterceptor performanceMonitorInterceptor() {
+        return new PerformanceMonitorInterceptor(true);
+    }
+
+    @Bean
+    public Advisor performanceMonitorAdvisor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("puzzle.sudoku.configs.PerformanceMonitor.monitor()");
+        return new DefaultPointcutAdvisor(pointcut, performanceMonitorInterceptor());
+    }
+}
