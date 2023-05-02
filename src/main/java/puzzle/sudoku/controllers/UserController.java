593ad97ad4bea3ccf205d64b9b5108a979d53679
mishoto <mihail.dev.io@gmail.com>
2023-05-02 09:13:07 +0300

    add RequestBodyValidator

--- src/main/java/puzzle/sudoku/controllers/UserController.java
+++ src/main/java/puzzle/sudoku/controllers/UserController.java
@@ -6,25 +6,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
+import puzzle.sudoku.exceptions.SudokuUnresolvableException;
 import puzzle.sudoku.models.request.SudokuSubmitRequest;
 import puzzle.sudoku.models.response.SudokuSolvedResponse;
 import puzzle.sudoku.services.UserService;
+import puzzle.sudoku.validators.RequestBodyValidator;
 
 @RestController
 public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    private final RequestBodyValidator requestBodyValidator;
+
+    public UserController(UserService userService, RequestBodyValidator requestBodyValidator) {
         this.userService = userService;
+        this.requestBodyValidator = requestBodyValidator;
     }
 
     @PostMapping(path = "/solve")
     public ResponseEntity<SudokuSolvedResponse> userSubmit(@Valid @RequestBody SudokuSubmitRequest sudokuSubmitRequest) {
+        requestBodyValidator.validate(sudokuSubmitRequest.getPuzzle());
         SudokuSolvedResponse response = userService.sudokuSolve(sudokuSubmitRequest);
         if(response.getSolution().length == 1){
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "The puzzle is not valid");
+            throw new SudokuUnresolvableException("Sudoku is unresolvable, please provide a valid sudoku puzzle");
         }
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
